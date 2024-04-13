903bcb7ab6cc13998d2342aa3dd4fde24232aa29
markusgemeinder <190774@gmx.de>
2024-04-13 09:06:37 +0200

    Next.js 14 Tutorial 53 Dynamic Rendering (see example /about)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,10 @@
-'use client';
+// 'use client';
+import { cookies } from 'next/headers';
 
 export default function AboutPage() {
+  const cookieStore = cookies();
+  const theme = cookieStore.get('theme');
+  console.log(theme);
   console.log('About server component');
   return <h1>About Page {new Date().toLocaleTimeString()}</h1>;
 }
