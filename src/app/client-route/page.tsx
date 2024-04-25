a89f4577c65b88bb98ac1486c5327ea0d7179125
markusgemeinder <190774@gmx.de>
2024-04-25 19:32:54 +0200

    Next.js 14 Tutorial 59 Client-only Code

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -1,14 +1,8 @@
 'use client';
 
-import { serverSideFunction } from '@/utils/server-utils';
+import { clientSideFunction } from '@/utils/client-utils';
 
 export default function ClientRoutePage() {
-  console.log('Client route rendered');
-  const result = serverSideFunction();
-  return (
-    <>
-      <h1>ClientRoutePage</h1>
-      <p>{result}</p>
-    </>
-  );
+  const result = clientSideFunction();
+  return <h1>Client route {result}</h1>;
 }
--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,8 +1,10 @@
+import { clientSideFunction } from '@/utils/client-utils';
 import { serverSideFunction } from '@/utils/server-utils';
 
 export default function ServerRoutePage() {
   console.log('Server route rendered');
   const result = serverSideFunction();
+  const clientResult = clientSideFunction();
   return (
     <>
       <h1>ServerRoutePage</h1>
